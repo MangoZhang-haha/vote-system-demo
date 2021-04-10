1da50afc46cae013dd9d8737167ee858cf0172c5
zql <549797834@qq.com>
2021-04-10 20:35:01 +0800

    c

---
+++ src/main/java/com/mapper/NoticeVoteMapper.java
@@ -0,0 +1,7 @@
+package com.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.domain.NoticeVote;
+
+public interface NoticeVoteMapper extends BaseMapper<NoticeVote> {
+}
